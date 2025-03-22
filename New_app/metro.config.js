19c6f1accd5134fd0f10d746ae4cbeff609be311
Franz kingstein N <franzkingstein@gmail.com>
2025-03-23 00:39:40 +0530

    Add files via upload

---
+++ New_app/metro.config.js
@@ -0,0 +1,10 @@
+/**
+ * Metro configuration for React Native
+ * https://facebook.github.io/metro/docs/configuration
+ */
+module.exports = {
+    resolver: {
+      sourceExts: ['jsx', 'js', 'ts', 'tsx'], // Ensure Metro recognizes these file types
+    },
+  };
+  
